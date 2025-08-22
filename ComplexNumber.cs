3a66cace2f0d6de93abcb2cbd7fca88e958e0d0e
Barcode <nicktarvestad@yahoo.com>
2025-08-22 18:43:17 -0500

    MandelbrotViewer v0.0.0
    This "MandelbrotViewer" is an interactive viewer to explore the Mandelbrot Set Fractal. This is built using .NET and C#. This program allows the user to change the parameters of the displayed Mandelbrot, zoom in on it, and save pictures of the current display.
    
    Created by Nick "Barcode" Tarvestad 8/22/2025.

---
+++ ComplexNumber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandelbrotViewerDotNet;
+public class ComplexNumber : IDisposable
+{
+	public double A { get; set; }
+	public double B { get; set; }
+
+	public ComplexNumber(double a, double b)
+	{
+		A = a;
+		B = b;
+	}
+
+	public double Modulus()
+	{
+		return Math.Sqrt(A*A + B*B);
+	}
+
+	public static ComplexNumber operator +(ComplexNumber f, ComplexNumber s)
+	{
+		return new ComplexNumber(f.A + s.A, f.B + s.B);
+	}
+
+	public static ComplexNumber operator *(ComplexNumber f, ComplexNumber s)
+	{
+		var a = f.A;
+		var b = f.B;
+		var c = s.A;
+		var d = s.B;
+
+		return new ComplexNumber((a * c) - (b * d), (a * d) + (b * c)); // This uses the "Foil" method of multiplying complex numbers.
+	}
+
+	private bool disposedValue;
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!disposedValue)
+		{
+			if (disposing)
+			{
+				// TODO: dispose managed state (managed objects)
+			}
+
+			// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+			// TODO: set large fields to null
+			disposedValue = true;
+		}
+	}
+
+	// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+	// ~ComplexNumber()
+	// {
+	//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+	//     Dispose(disposing: false);
+	// }
+
+	public void Dispose()
+	{
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MandelbrotViewerDotNet
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+
+	public class ImageWizard
+	{
+		BitmapSource bitmap;
+
+		public int ImageWidth { get; set; } = 2600;
+		public int ImageHeight { get; set; } = 1600;
+
+		public ImageSource GenerateImage(int width, int height, ComplexNumber center, double step, int[] palette)
+		{
+			bitmap output = new WriteableBitmap(ImageWidth, ImageHeight, 96, 96, PixelFormats.Bgra32, null);
+		}
+	}
+
+	public class Iterator
+	{
+
+	}
+
+	public static class MrCalc
+	{
+		public static int Iterate(ComplexNumber c, int limit)
+		{
+			var z = new ComplexNumber(0, 0);
+
+			for (var i = 0; i < limit; i++)
+			{
+				z = (z * z) + c;
+				if (z.Modulus() >= 2)
+				{
+					return i;
+				}
+			}
+
+			return limit;
+		}
+	}
+}
